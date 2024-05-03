19b6598ab1354de3a2c25401832f2e348f3b5d09
Giovana Coutinho <ctnho1999@gmail.com>
2024-05-02 22:39:16 -0300

    Add aula java

--- project/src/App.java
+++ project/src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+     
+        String primeiroNome = "Giovana";
+        String segundoNome = "Coutinho";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
